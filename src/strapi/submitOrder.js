8c1a119df12b78597dd36e30cb0719af5519c36f
alexis21o8 <menghan821@gmail.com>
2021-11-23 15:19:56 +0800

    slight error fix

--- src/strapi/submitOrder.js
+++ src/strapi/submitOrder.js
@@ -2,11 +2,20 @@ import axios from "axios";
 import url from "./URL";
 
 async function submitOrder({name,total,items,stripeTokenId,userToken}){
-    const response = await axios.post(`${url}/orders`,{name,total,items,stripeTokenId},{
-        headers:{
-            Authorization: `Bearer ${userToken}`
+    const response = await axios.post(`${url}/orders`,
+        {
+            name,
+            total,
+            items,
+            stripeTokenId
         },
-    }).catch(error => console.log(error));
+        {
+            headers:{
+                Authorization: `Bearer ${userToken}`
+            }
+        }
+    )
+    .catch(error => console.log(error));
     return response;
 }
 
